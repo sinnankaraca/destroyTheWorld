5c3321bc3f8498050fda7ee8cdc7905ced70e1c2
Sinan Karaca <sinnankaraca@icloud.com>
2017-11-08 22:05:06 +0300

    Initial Commit

---
+++ destroyTheWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  destroyTheWorld
+//
+//  Created by Sinan Karaca on 8.11.2017.
+//  Copyright © 2017 Sinan Karaca. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
